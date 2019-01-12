7da3a61576dba814768057a1a20cc51028dc453e
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2019-01-12 10:57:35 +0000

    Update

--- publish.sh
+++ publish.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+./prettier.sh
+
+# Set user
+git config user.name "Artemij Fedosejev"
+git config user.email "artemij.fedosejev@gmail.com"
+
 # Commit and push
 git checkout master
 git add .
